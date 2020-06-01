21e37930a697bf429e37942c9b00e9fbcee1eb13
coope <cooperphil16@gmail.com>
2020-05-31 22:16:11 -0400

    homepage

--- api.py
+++ api.py
@@ -4,9 +4,14 @@ from deck_app import deck_scraper
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/<path:url>', methods=["GET"])
-def main(url):
+
+
+@app.route('/scrape/<path:url>', methods=["GET"])
+def scrape(url):
 
     return deck_scraper.main_app(url)
+@app.route('/')
+def home():
+    return "<h1> Welcome </h1>"
 
 app.run()
