43368e0f8ea1cbceed6cdf7b1ce3c76f3e40028b
TheArchwizard <33990085+TheArchwizard@users.noreply.github.com>
2021-03-07 21:17:52 -0500

    Add files via upload

---
+++ scraping/static/js/request.js
@@ -0,0 +1,31 @@
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const url = 'https://deck-scraper.herokuapp.com/names/';
+var tree = document.createDocumentFragment();
+var averagecmc = 0;
+
+async function submitted(event)
+{
+    event.preventDefault()
+    const response = await fetch(url + q.value);
+
+    const responsedata = await response.json();
+    const cleaneddata = responsedata.values;
+    console.log(cleaneddata);
+
+
+    for(x in cleaneddata)
+    {
+
+        var div = document.createElement("div");
+        div.setAttribute("id", cleaneddata[x].name);
+        div.appendChild(document.createTextNode(cleaneddata[x].num + "x "));
+        div.appendChild(document.createTextNode(cleaneddata[x].name));
+        tree.append(div);
+    }
+
+    document.getElementById("main").appendChild(tree);
+
+    const newdata = JSON.stringify(cleaneddata);
+}
+f.addEventListener('submit', submitted);
