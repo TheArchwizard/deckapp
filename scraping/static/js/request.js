75fdeeb1b69e7196125e8611fb83745031b1214d
TheArchwizard <33990085+TheArchwizard@users.noreply.github.com>
2021-03-12 22:05:57 -0500

    Update request.js

--- scraping/static/js/request.js
+++ scraping/static/js/request.js
@@ -1,37 +1,65 @@
 const f = document.getElementById('form');
 const q = document.getElementById('query');
-const url = 'https://deck-scraper.herokuapp.com/names/';
+const namesUrl = 'https://deck-scraper.herokuapp.com/names/';
+const scrapeUrl = 'https://deck-scraper.herokuapp.com/scrape/';
 var tree = document.createDocumentFragment();
-var averagecmc = 0;
 
 async function submitted(event)
 {
     const myNode = document.getElementById("main");
 
-    while (myNode.firstChild) 
+    while (myNode.firstChild)
     {
     myNode.removeChild(myNode.lastChild);
     }
-    
-    event.preventDefault()
-    const response = await fetch(url + q.value);
+
+
+    event.preventDefault();
+
+    const response2 = await fetch(scrapeUrl + q.value);
+    const responsedata2 = await response2.json();
+
+    const cleaneddata2 = responsedata2.data;
+
+    console.log(cleaneddata2);
+
+    for(x in cleaneddata2)
+    {
+        try
+        {
+        console.log(cleaneddata2[x].image_uris.png);
+        }
+
+        catch(err)
+        {
+        for(y in cleaneddata2[x]){
+        console.log(y.card_faces);
+        }
+
+        }
+
+    }
+
+
+
+    const response = await fetch(namesUrl + q.value);
 
     const responsedata = await response.json();
     const cleaneddata = responsedata.values;
     console.log(cleaneddata);
-
 
     for(x in cleaneddata)
     {
 
         var div = document.createElement("div");
         div.setAttribute("id", cleaneddata[x].name);
+
         div.appendChild(document.createTextNode(cleaneddata[x].num + "x "));
         div.appendChild(document.createTextNode(cleaneddata[x].name));
         tree.append(div);
     }
-
     document.getElementById("main").appendChild(tree);
+
 
     const newdata = JSON.stringify(cleaneddata);
 }
